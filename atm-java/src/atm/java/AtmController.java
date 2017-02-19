9575abb823a94b32cecea1c6773a97d9646350b7
illmatictime <jerrylanderos@gmail.com>
2017-02-18 16:43:15 -0800

    Merge branches 'Account_Branch', 'develop' and 'feature/account-model' of https://github.com/MiguelHuerta88/atm-system into Account_Branch
    # Conflicts:
    #	Contributors.txt
    
    Changed variables to fit convention.
    Started Controller and View.

---
+++ atm-java/src/atm/java/AtmController.java
@@ -0,0 +1,29 @@
+package atm.java;
+
+/**
+ * Created by Darkness on 2/17/2017.
+ */
+public class AtmController {
+    private Account model;
+    private AtmView view;
+
+    public AtmController(Account model, AtmView view){
+        this.model = model;
+        this.view = view;
+    }
+
+    public void displayBalance() {
+        double balance = model.getBalance();
+
+        view.displayBalance(balance);
+    }
+
+    public void displayFirstName(){
+        String name = model.getFirstName();
+
+        view.displayFirstName(name);
+
+    }
+
+
+}
---
+++ atm-java/src/atm/java/AtmView.java
@@ -0,0 +1,15 @@
+package atm.java;
+
+/**
+ * Created by Darkness on 2/18/2017.
+ */
+public class AtmView {
+    public void displayBalance(double balance) {
+        System.out.println("Your balance is: " + balance);
+    }
+
+    public void displayFirstName(String name){
+        System.out.print("Hello! Welcome " + name + "!");
+    }
+}
+
