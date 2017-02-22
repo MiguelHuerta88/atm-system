8b803f270674ebab0933724a4a13b0ba006ab58e
illmatictime <jerrylanderos@gmail.com>
2017-02-21 18:32:19 -0800

    Add files via upload

---
+++ src/CheckingAccount.java
@@ -0,0 +1,44 @@
+/**
+ * Created by MiguelHuerta on 2/17/17.
+ */
+public class CheckingAccount extends BaseAccount
+{
+    public final double WITHDRAW_MAX = 500.00;
+
+    public CheckingAccount(int account, double initBalance, int typeId, String lastAccess)
+    {
+        // call the super method on the abstract class.
+        super(account, initBalance, typeId, lastAccess);
+    }
+
+    public void depositAmount(double add)
+    {
+        if(add > 0)
+        {
+            balance +=add;
+        }
+    }
+
+    public void withdrawAmount(double amount)
+    {
+
+        if ((balance > 0 && amount < balance) && (amount > 0 && amount <= WITHDRAW_MAX) )
+        {
+            balance -= amount;
+        }
+    }
+
+    /**
+     * toString method
+     *
+     * @return String
+     */
+    public String toString()
+    {
+        // display how the account information should be displayed.
+        //String toString = super.toString();
+
+        return super.toString();
+    }
+}
+
